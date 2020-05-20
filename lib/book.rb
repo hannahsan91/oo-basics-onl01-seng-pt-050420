0adab04970621bcca482b08519301df88d0825dd
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-20 01:20:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -101,19 +101,16 @@ It looks like our turn_page method doesn't really need to do much aside from pri
 # book.rb
 class Book
   attr_accessor :author, :page_count, :genre
-
+  attr_reader :title
+ 
   def initialize(title)
     @title = title
   end
-
-  def title
-    @title
-  end
-
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+ 
 end
 
 book = Book.new("Some Title")
